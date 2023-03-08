06c4cc3823dd871936816d23e42e367e55a24eef
Adam Gragnic <adamgragnic@gmail.com>
2023-03-08 12:20:45 +0100

    Ajout de bootstrap

--- app/index.php
+++ app/index.php
@@ -5,8 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" type="text/css" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/twbs/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
     <?php phpinfo(); ?>
+
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.bundle.js"></script>
+    <script src="js/scripts.js"></script>
 </body>
 </html>
